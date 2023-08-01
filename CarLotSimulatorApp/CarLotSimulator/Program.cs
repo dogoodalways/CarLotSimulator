cbc33fc0fac8431280259d74927ae2b90a759466
Yosef <yosefamiami@gmail.com>
2023-08-01 12:07:13 -0400

    CarLot exercise

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Xml.Schema;
 
 namespace CarLotSimulator
 {
@@ -8,15 +10,64 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
+            //Done - Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
 
-            //Now that the Car class is created we can instanciate 3 new cars
+            //Now that the Car class is created
+            //we can instanciate 3 new cars
+            // below is the constructor. In the first example
+            // its a method that accepts the properties. It does 
+            // this because the properties become the parameters of the
+            // of the method which is the constructor. 
+            // Instantiation 1 using dot notation. 
+            CarLot carLot= new CarLot();
+            Car YosefsCar = new Car();
+            YosefsCar.Make = "Tesla";
+            YosefsCar.Model = "ModelS";
+            YosefsCar.Year = 2022;
+            YosefsCar.EngineNoise = "mmmmm";
+            YosefsCar.HonkNoise = "beep";
+            YosefsCar.IsDriveable = true;
+            carLot.ParkingLot.Add(YosefsCar);
+            Console.WriteLine($"The number of cars in the lot is {CarLot.numberOfCars}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Car RivkysCar = new Car()
+            {
+                Year = 2019,
+                Make = "Ford",
+                Model = "Transit 350",
+                EngineNoise = "vroom",
+                HonkNoise = "beep",
+                IsDriveable = true,
+            };
+                   
+                carLot.ParkingLot.Add(RivkysCar);
+            Console.WriteLine($"The number of cars in the lot is {CarLot.numberOfCars}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            // Instantiation 3.
+            Car aTradingCar = new Car(2021, "Honda", "Pilot", "vroom", "beep", true);
+
+            CarLot.numberOfCars++;
+            Console.WriteLine($"The number of cars in the lot is {CarLot.numberOfCars}");
+
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            
+
+
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+
 
             //*************BONUS*************//
 
